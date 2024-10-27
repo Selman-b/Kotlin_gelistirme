04dea4c7cbda52b78e2621d8dd00972af8fe918e
Selman <selmanb0161@gmail.com>
2024-10-27 17:47:44 +0300

    Kotlin  uygulama

---
+++ app/src/main/java/com/example/uygulama_denemesi1/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.uygulama_denemesi1
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.uygulama_denemesi1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+    override fun onStop() {
+        super.onStop()
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    fun SonrakiSayfa(view : View){
+        val kullaniciGirdisi = binding.editText.text.toString()
+        binding.textView3.text="Isim : ${kullaniciGirdisi}"
+
+    }
+}
